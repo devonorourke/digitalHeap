6c7b23021bc052cece267048f01863042035c880
Devon O'Rourke <devon.orourke@gmail.com>
2017-09-23 21:55:38 -0400

    Create lesson1.m

---
+++ hssci/lesson1.m
@@ -0,0 +1,9 @@
+# Jupyter Notebook
+So recall that in the [introductory lesson](https://github.com/devonorourke/digitalHeap/blob/master/hssci/lesson0.md) we downloaded [Anaconda](https://www.anaconda.com/what-is-anaconda/), which contained [Python](https://www.python.org/about/gettingstarted/) as well as many other unnamed software packages. Basically, we downloaded some soft of Russian nested dolls of software. As mentioned earlier, one other advantage of doing this big installation was that we could quickly manage future downloads of additoinal software we might want. And even before we get our first lesson rolling we have to do just that - we need to download a program called [Jupyter Notebook](http://jupyter.readthedocs.io/en/latest/index.html)!  
+
+You might be thinking two things:  
+> 1) How many times are we going to be downloading software? Is that all this course is about?
+Answer: You will download software routinely. This is not what the course is about.  
+> 2) Why am I downloading *this particular software*?
+Answer: Better question! We're using this software because it's a digital notebook which lets you simultaneously write the code you want to perform a function, execute that code within the program, see the output of that code, **AND** let's you separate out all your code, output, and basic note taking into convenient blocks. It does a lot of useful stuff, okay?   
+
